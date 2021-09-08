ad0d075b32b4d9b770c6cee6a968c37b1a4f99b5
Arushi Anand <arushi474@gmail.com>
2021-09-08 21:00:00 +0530

    Create Remove Consecutive Duplicates.cpp

---
+++ Character Arrays and 2D Arrays Assignments/Remove Consecutive Duplicates.cpp
@@ -0,0 +1,50 @@
+/*
+Remove Consecutive Duplicates
+For a given string(str), remove all the consecutive duplicate characters.
+Example:
+Input String: "aaaa"
+Expected Output: "a"
+
+Input String: "aabbbcc"
+Expected Output: "abc"
+ Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+Output Format:
+The only line of output prints the updated string.
+Note:
+You are not required to print anything. It has already been taken care of.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+aabccbaa
+Sample Output 1:
+abcba
+Sample Input 2:
+xxyyzxx
+Sample Output 2:
+xyzx
+*/
+void removeConsecutiveDuplicates(char input[]) {
+    
+    int count=0;
+    for(int i=0;input[i]!='\0';i++){
+       count++;
+    }
+    char last=input[0];
+    int j=1,i=1;
+    while(i<count){
+        if(input[i]==last) {
+           i++;  
+        }  
+        else {
+            input[j]=input[i];
+            last=input[i];
+            i++;
+            j++;
+            
+        }
+} input[j]='\0';
+}
